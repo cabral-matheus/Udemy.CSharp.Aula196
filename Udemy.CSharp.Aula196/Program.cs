b3c19736e6958082ed963e307e17d8a3ece8aa4a
Matheus Cabral <matheuscabraldesousa@gmail.com>
2023-06-19 16:09:20 -0300

    First commit

---
+++ Udemy.CSharp.Aula196/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Udemy.CSharp.Aula196
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\mathe\OneDrive\Documentos\Projetos C#\Udemy.CSharp.Aula196\file1.txt";
+            FileStream fs = null;
+            StreamReader sr = null;
+
+            try
+            {
+                fs = new FileStream(path, FileMode.Open);
+                sr = new StreamReader(fs);
+
+                while (!sr.EndOfStream)
+                {
+                    string line = sr.ReadLine();
+                    Console.WriteLine(line);
+                }
+            } catch (IOException e)
+            {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+            } finally
+            {
+                if (fs != null) fs.Close();
+                if (sr != null) sr.Close();
+            }
+        }
+    }
+}
